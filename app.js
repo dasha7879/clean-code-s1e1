e397869a35e277fa0256fa8bc3e4795697d36a0f
dasha7879 <belka7879@gmail.com>
2022-10-31 22:19:53 +0300

    refactor: edit point 1.3 Quotation marks in HTML/CSS document

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ var createNewTaskElement=function(taskString){
     var deleteButtonImg=document.createElement("img");//delete button image
 
     label.innerText=taskString;
-    label.className='task';
+    label.className="task";
 
     //Each elements, needs appending
     checkBox.type="checkbox";
@@ -45,7 +45,7 @@ var createNewTaskElement=function(taskString){
     editButton.className="button button__edit";
 
     deleteButton.className="button button__delete";
-    deleteButtonImg.src='./remove.svg';
+    deleteButtonImg.src="./remove.svg";
     deleteButtonImg.className= "img";
     deleteButton.appendChild(deleteButtonImg);
 
@@ -84,7 +84,7 @@ var editTask=function(){
 
     var listItem=this.parentNode;
 
-    var editInput=listItem.querySelector('input[type=text]');
+    var editInput=listItem.querySelector("input[type=text]");
     var label=listItem.querySelector("label");
     var editBtn=listItem.querySelector(".button__edit");
     var containsClass=listItem.classList.contains("todo-list__item_edit-mode");
